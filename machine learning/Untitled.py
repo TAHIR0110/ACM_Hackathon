148f8a7e566dfd35ee009028649c6c8d25003f63
aa <142321494+AnandRajaM@users.noreply.github.com>
2024-01-17 15:14:20 +0530

    Update Untitled.py

--- machine learning/Untitled.py
+++ machine learning/Untitled.py
@@ -1,9 +1,12 @@
 #!/usr/bin/env python
 # coding: utf-8
+#pip install serpapi
+#pip install google-search-results
+
 
 # In[61]:
 
-
+from serpapi import GoogleSearch
 import numpy as np
 import pandas as pd
 from sklearn.model_selection import train_test_split
@@ -57,5 +60,37 @@ label_encoder.inverse_transform(top_3_classes)
 
 
 
+inp_job = top_3_classes[0]
 
+params = {
+  "api_key": "74667d45acf8d73e71f442fcea119219a9c88b09bb187292159a3fc88235289c",
+  "engine": "google_jobs",
+  "google_domain": "google.com",
+  "q": str(inp_job)
+}
+
+search = GoogleSearch(params)
+result = search.get_dict()
+
+
+Job_ID = []
+for job_result in result['jobs_results']:
+    Job_ID.append((job_result['job_id'],job_result['company_name'],job_result['title']))
+print('1---------------------------------------------------------------1')
+
+for job_id in Job_ID:
+    params = {
+        "api_key": "74667d45acf8d73e71f442fcea119219a9c88b09bb187292159a3fc88235289c",
+        "engine": "google_jobs_listing",
+        "q": str(job_id[0])
+  }
+    search = GoogleSearch(params)
+    results = search.get_dict()
+    try:
+      print('---------------------------------------------------------------')
+      print(job_id[1])
+      print(job_id[2])
+      print(results['apply_options'])
+    except:
+       continue
 
